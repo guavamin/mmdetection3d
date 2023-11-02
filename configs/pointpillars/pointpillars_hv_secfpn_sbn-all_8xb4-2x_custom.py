78c24050a8b87cb151c45508eeca928eb518e0fc
guavamin <kl3435@columbia.edu>
2023-11-02 11:23:34 -0400

    update model config

---
+++ configs/pointpillars/pointpillars_hv_secfpn_sbn-all_8xb4-2x_custom.py
@@ -0,0 +1,6 @@
+_base_ = [
+    '../_base_/models/pointpillars_hv_fpn_custom.py',
+    '../_base_/datasets/custom.py',
+    '../_base_/schedules/schedule-2x.py', 
+    '../_base_/default_runtime.py'
+]
